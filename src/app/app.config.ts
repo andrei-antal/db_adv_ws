9c8dbb4bd0512ddf08d1d5840d09e29196b3ecf3
Andrei Antal <antal.andrei@icloud.com>
2024-10-23 23:05:44 +0300

    done challenge 16

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -6,11 +6,13 @@ import {
   withComponentInputBinding,
 } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { isAuthenticatedGuard } from './guards/is-authenticated.guard';
 
 const routes: Route[] = [
   { path: '', component: HomeComponent },
   {
     path: 'movies',
+    canActivate: [isAuthenticatedGuard],
     loadChildren: () =>
       import('./movies/movies.routes').then((m) => m.movieRoutes),
   },
