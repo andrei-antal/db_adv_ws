b341c9a566b694f74f33fa0e1a96b76daa50395c
Andrei Antal <antal.andrei@icloud.com>
2024-10-23 10:18:01 +0300

    done challenge 12

--- src/app/movies/services/movie.service.ts
+++ src/app/movies/services/movie.service.ts
@@ -3,13 +3,20 @@ import { HttpClient } from '@angular/common/http';
 import {
   BehaviorSubject,
   Observable,
+  Subject,
   catchError,
+  interval,
+  merge,
   of,
+  shareReplay,
   switchMap,
+  takeUntil,
   tap,
 } from 'rxjs';
 import { EMPTY_MOVIE, Movie } from '../model/movie';
 import { environment } from '../../../environments/environment';
+
+const REFRESH_INTERVAL = 3000;
 
 @Injectable({
   providedIn: 'root',
@@ -19,15 +26,31 @@ export class MovieService {
   readonly #movieApi = `${environment.apiUrl}/movies`;
   readonly #genreApi = `${environment.apiUrl}/genres`;
   readonly #reload = new BehaviorSubject<void>(undefined);
+  readonly #clearCache = new Subject<void>();
+  #cache!: Observable<Movie[]>;
 
-  getMovies(searchTerm = ''): Observable<Movie[]> {
-    return this.#reload.pipe(
-      switchMap(() =>
-        this.#http
-          .get<Movie[]>(`${this.#movieApi}?q=${searchTerm.trim()}`)
-          .pipe(catchError(() => of([])))
-      )
-    );
+  getMovies(
+    searchTerm = '',
+    refreshInterval = REFRESH_INTERVAL
+  ): Observable<Movie[]> {
+    const getMoviesApi = this.#http
+      .get<Movie[]>(`${this.#movieApi}?q=${searchTerm.trim()}`)
+      .pipe(catchError(() => of([])));
+
+    if (searchTerm !== '') {
+      return this.#reload.pipe(switchMap(() => getMoviesApi));
+    } else if (!this.#cache) {
+      this.#cache = merge(this.#reload, interval(refreshInterval)).pipe(
+        switchMap(() => getMoviesApi),
+        shareReplay(1),
+        takeUntil(this.#clearCache)
+      );
+    }
+    return this.#cache;
+  }
+
+  clearCache(): void {
+    this.#clearCache.next();
   }
 
   updateComment(movieId: string, newComment: string): Observable<Movie> {
