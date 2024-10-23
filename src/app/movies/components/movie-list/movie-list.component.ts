733e86c42f3ab4dc26673e457697163656567ea6
Andrei Antal <antal.andrei@icloud.com>
2024-10-23 10:30:25 +0300

    done challenge 13

--- src/app/movies/components/movie-list/movie-list.component.ts
+++ src/app/movies/components/movie-list/movie-list.component.ts
@@ -6,10 +6,21 @@ import {
   RatingUpdate,
 } from '../movie-item/movie-item.component';
 import { MovieService } from '../../services/movie.service';
-import { debounceTime, startWith, switchMap } from 'rxjs';
+import {
+  debounceTime,
+  distinctUntilChanged,
+  filter,
+  fromEvent,
+  map,
+  scan,
+  startWith,
+  switchMap,
+} from 'rxjs';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { toSignal } from '@angular/core/rxjs-interop';
+import { TmdbService } from '../../services/tmdb.service';
+import { Movie } from '../../model/movie';
 
 @Component({
   selector: 'ngm-movie-list',
@@ -26,6 +37,18 @@ import { toSignal } from '@angular/core/rxjs-interop';
 })
 export class MovieListComponent {
   readonly #movieService = inject(MovieService);
+  readonly #tmdbService = inject(TmdbService);
+
+  pageByScroll$ = fromEvent(window, 'scroll').pipe(
+    map(() => window.scrollY),
+    filter(
+      (current) => current >= document.body.clientHeight - window.innerHeight
+    ),
+    debounceTime(200),
+    distinctUntilChanged(),
+    startWith(1),
+    scan((page) => page + 1)
+  );
 
   searchField = new FormControl('');
   movies$ = toSignal(
@@ -33,6 +56,16 @@ export class MovieListComponent {
       startWith(''),
       debounceTime(300),
       switchMap((searchTerm) => this.#movieService.getMovies(searchTerm || ''))
+    )
+  );
+
+  tmdbMovies$ = toSignal(
+    this.pageByScroll$.pipe(
+      switchMap((page) => this.#tmdbService.getMovies(page)),
+      scan<Movie[], Movie[]>(
+        (existingMovies, newMovies) => [...existingMovies, ...newMovies],
+        []
+      )
     )
   );
 
